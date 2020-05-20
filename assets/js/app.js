de21cfd6ae3495c946d2952e4a81214f80b86faa
Bruno <bruno.predot@gmail.com>
2020-05-20 12:37:51 +0200

    create homePage, customersPage & InvoicesPage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom'
+import {HashRouter} from 'react-router-dom';
+import {Switch, Route} from 'react-router'
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+import CustomersPage from "./pages/CustomersPage";
+import InvoicesPage from "./pages/InvoicesPage";
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
@@ -11,9 +18,17 @@ console.log('Hello ! Welcome to my app !');
 
 function App() {
     return(
-        <div className={"App"}>
-            <h1>Bonjour bonjour!</h1>
-        </div>
+        <HashRouter>
+            <Navbar/>
+            <main className="container pt-5">
+                <Switch>
+                    <Route path="/" component={HomePage}/>
+                    <Route path="/clients" component={CustomersPage}/>
+                    <Route path="/factures" component={InvoicesPage}/>
+                    <Route path="" component={NotFound} />
+                </Switch>
+            </main>
+        </HashRouter>
     )
 }
 
---
+++ assets/js/pages/InvoicesPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InvoicesPage = (props) => {
+    return (
+        <div>
+            <h1>Liste des factures</h1>
+        </div>
+    );
+};
+
+export default InvoicesPage;
