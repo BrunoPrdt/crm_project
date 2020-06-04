860e50988a303817b2de2b8073994eeb30f802c1
Bruno <bruno.predot@gmail.com>
2020-06-04 19:16:32 +0200

    add form on loginPage

--- assets/js/pages/LoginPage.jsx
+++ assets/js/pages/LoginPage.jsx
@@ -4,6 +4,16 @@ const LoginPage = () => {
     return (
         <>
             <h1>Connexion à l'application</h1>
+            <form action="">
+                <div className="form-group">
+                    <label htmlFor="username">Adresse email</label>
+                    <input type="email" placeholder="Adresse email de connexion" name="username" id="username" className="form-control"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Mot de passe</label>
+                    <input type="password" placeholder="Mot de passe" name="password" id="password" className="form-control"/>
+                </div>
+            </form>
         </>
     );
 };
