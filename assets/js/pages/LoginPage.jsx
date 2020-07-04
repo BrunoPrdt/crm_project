fb17b0f9f713699ea1ac3cb861c525f28a7b1165
BrunoPrdt <bruno.predot@gmail.com>
2020-07-04 10:00:07 +0200

    add onChangeon LoginPage

--- assets/js/pages/LoginPage.jsx
+++ assets/js/pages/LoginPage.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const LoginPage = () => {
+    const [credentials, setCredentials] = useState({
+        username: "Toto Lasticot",
+        password: "koko le koala"
+    });
+
+    const handleChange = (event)=>{
+        const name = event.currentTarget.name;
+        const value = event.currentTarget.value;
+        setCredentials({...credentials, [name]:value});
+    };
+
     return (
         <>
             <h1>Connexion à QuickFactures</h1>
             <form action="">
                 <div className="form-group">
                     <label htmlFor="username">Adresse email</label>
-                    <input type="email" placeholder="Adresse email de connexion" name="username" id="username" className="form-control"/>
+                    <input
+                        onChange={handleChange}
+                        value={credentials.username}
+                        type="email"
+                        placeholder="Adresse email de connexion"
+                        name="username"
+                        id="username"
+                        className="form-control"
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Mot de passe</label>
-                    <input type="password" placeholder="Mot de passe" name="password" id="password" className="form-control"/>
+                    <input
+                        onChange={handleChange}
+                        value={credentials.password}
+                        type="password"
+                        placeholder="Mot de passe"
+                        name="password"
+                        id="password"
+                        className="form-control"
+                    />
                 </div>
                 <div className="form-group">
                     <button type="submit" className="btn btn-success">Se connecter</button>
