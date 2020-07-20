26d29643acc526b81e268700a8e6fae721bafffc
BrunoPrdt <bruno.predot@gmail.com>
2020-07-20 07:52:24 +0200

    add dotenv & environement variable

--- assets/js/services/Config.js
+++ assets/js/services/Config.js
@@ -1,6 +1,7 @@
 import jwtDecode from 'jwt-decode';
 
-export const SERVER_URL = "https://127.0.0.1:8000";
+// export const SERVER_URL = "https://127.0.0.1:8000";
+export const SERVER_URL = process.env.API_URL;
 
 /**
  *
